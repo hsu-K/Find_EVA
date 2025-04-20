3e6779037835cf8cd02fe026479729a33509e75a
kou-ming <t0937993806@gmail.com>
2025-04-20 21:21:24 +0800

    unsafe terminateThread

--- Launcher/GlobalState.cpp
+++ Launcher/GlobalState.cpp
@@ -1,4 +1,5 @@
 #include "GlobalState.hpp"
+#include <future>
 
 std::shared_ptr<GlobalState> GlobalState::_instance = nullptr;
 
@@ -16,10 +17,26 @@ bool GlobalState::removeThread(size_t index)
 {
 	std::unique_lock<std::mutex> lock(threadPoolMutex);
 	if (index < threadPool.size()) {
-
+		// 先嘗試取消線程的 I/O 操作
+		HANDLE threadHandle = threadNativeHandles[index];
 
 		if (threadPool[index]->joinable()) {
-			threadPool[index]->join();
+			/*threadPool[index]->join();*/
+			std::future<void> future = std::async(std::launch::async, [&]() {
+				threadPool[index]->join();
+				});
+
+			// 等待線程結束，但最多等待 3 秒
+			if (future.wait_for(std::chrono::seconds(1)) == std::future_status::timeout) {
+				std::cerr << "警告: 線程 " << index << " 無法在超時時間內關閉" << std::endl;
+				// 強制終止線程 (不安全!)
+				DWORD exitCode = 0;
+				if (!TerminateThread(threadHandle, exitCode)) {
+					std::cerr << "強制終止線程失敗: " << GetLastError() << std::endl;
+				}
+				// 注意：這裡無法安全地 detach，可能會導致資源洩漏
+				// 但為了不阻塞程序，我們必須繼續
+			}
 		}
 		threadPool.erase(threadPool.begin() + index);
 
